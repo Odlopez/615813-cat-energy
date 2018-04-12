b5ce5c5054a4d726cf784470db2a86808b71dd9e
Odlopez <Odlopez@inbox.ru>
2018-04-12 04:51:08 +0300

    Создает скрипт для подсвечивания "инвалидных" полей формы

---
+++ source/js/invalid-input.js
@@ -0,0 +1,17 @@
+var requiredIputs = document.querySelectorAll('input[required]');
+
+for (var i = 0; i < requiredIputs.length; i++) {
+  // можно использовать внутри цикла let вместо var и тогда не придется наворачивать лишнюю функцию
+  // борясь с замыканиями.
+  var x = function (i) {
+    requiredIputs[i].addEventListener('input', function() {
+
+    if (requiredIputs[i].value !== '') {
+      requiredIputs[i].classList.add('input-text--required');
+    } else {
+      requiredIputs[i].classList.remove('input-text--required');
+    }
+    })
+  };
+  x(i);
+}
