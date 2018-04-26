fcf825a4db9d663b5ec5e427aee3524d2f960458
Odlopez <Odlopez@inbox.ru>
2018-04-26 23:26:35 +0300

    Создает скрипт для слайдера на мобильной версии

---
+++ source/js/toggle-mobile.js
@@ -0,0 +1,27 @@
+const catBefore = document.querySelector(".example__image--before");
+const toggleSlider = document.querySelector(".toggle__slider");
+const wrapImage = document.querySelector(".example__image-wrap");
+const buttonCatBefore = document.querySelector(".toggle__label--before");
+const buttonCatAfter = document.querySelector(".toggle__label--after");
+
+//Проверяем, если расширение экрана - мобильное, и на данный момент худой кот видим, открываем толстого и скрываем худого.
+buttonCatBefore.addEventListener("click", function () {
+  const width = window.innerWidth;
+  if (width < 768 && wrapImage.classList.contains("js-visible") ) {
+    wrapImage.classList.toggle("js-visible");
+    catBefore.classList.toggle("js-hidden");
+    toggleSlider.classList.toggle("js-slide-right");
+  }
+});
+
+//Тут все наоборот.
+buttonCatAfter.addEventListener("click", function () {
+  const width = window.innerWidth;
+  if (width < 768 && !wrapImage.classList.contains("js-visible") ) {
+    wrapImage.classList.toggle("js-visible");
+    catBefore.classList.toggle("js-hidden");
+    toggleSlider.classList.toggle("js-slide-right");
+  }
+});
+
+//Еще нужно придумать, как удалять эти js-классы, если в одном сеансе перешли с мобильного на другое расширение экрана.
